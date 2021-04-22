6d0a12a174f2173c9b99f87ba48af16f47ab5ba0
Clement Atlan <atlan@latlan.esrf.fr>
2021-04-22 10:56:35 +0200

    add some update from notebook

--- multi_bcdi/3D_phase.py
+++ multi_bcdi/3D_phase.py
@@ -5,6 +5,40 @@ from utils import are_coplanar
 
 # TODO: Check if the compute 3D phase functions need normalized vectors or not
 
+def compute_3D_phase_V2(phases, q_vectors, support):
+    # First load the vectors of the measurement frame.
+    Q = [q1, q2, q3] = list(q_vectors.values())
+    Q = []
+    phi = []
+    for key in phases.keys():
+        Q.append(q_vectors[key])
+        phi.append(phases[key])
+    phi = np.array(phi)
+
+    # check if vectors are coplanar. If they are, they cannot describe the
+    # 3D space and then 3D phase cannot be computed
+    if are_coplanar(q1, q2, q3):
+        print("The given vectors are coplanar, this won't work.")
+        return None
+    Q_inverse = np.linalg.inv(np.array(Q))
+
+    # Get only the coordinates of the voxel within the support.
+    I, J, K = np.where(support > 0)
+
+    # Initialise the 3D phase.
+    shape = support.shape
+    phase_3D = np.zeros([shape[0], shape[1], shape[2], 3])
+
+    # Iteration over each voxel. 3D phase is computed for each voxel.
+    for i, j, k in zip(I.tolist(), J.tolist(), K.tolist()):
+
+        # Compute U values in the canonical basis.
+        U = np.dot(Q_inverse, phi[..., i, j, k])
+        phase_3D[i, j, k] = np.array(U)
+
+    return phase_3D
+
+
 def compute_3D_phase(phases, q_vectors, support):
 
     # First load the vectors of the measurement frame.
--- utils.py
+++ utils.py
@@ -39,7 +39,7 @@ def get_data_from_cxi(file, *items):
             data_dic["llkf"] = float(data["entry_1/image_1/process_1/results/" \
                                           "free_llk_poisson"][...])
 
-        if "llk" in items:
+        if "llk" in items:branch
             data_dic["llk"] = float(data["entry_1/image_1/process_1/results/" \
                                           "llk_poisson"][...])
 
@@ -50,3 +50,39 @@ def get_data_from_cxi(file, *items):
         print("[ERROR] An error occured while opening the file:", f,
               "\n", e.__str__())
         return None
+
+
+def center(data, com=None):
+    shape = data.shape
+    if com is None:
+        com =[round(c) for c in center_of_mass(data)]
+    centered_data = np.roll(data, shape[0] // 2 - com[0], axis=0)
+    centered_data = np.roll(centered_data, shape[1] // 2 - com[1], axis=1)
+    centered_data = np.roll(centered_data, shape[2] //2 - com[2], axis=2)
+
+    return centered_data
+
+
+def crop_at_center(data, final_shape=None):
+    if final_shape is None:
+        print("No final shape specified, did not proceed to cropping")
+        return data
+
+    shape = data.shape
+    final_shape = np.array(final_shape)
+
+    if not (final_shape <= data.shape).all():
+        print("One of the axis of the final shape is larger than " \
+              "the initial axis (initial shape: {}, final shape: {}).\n" \
+              "Did not proceed to cropping.".format(shape, tuple(final_shape)))
+        return data
+
+    c = np.array(shape) // 2
+    to_crop = final_shape // 2
+    plus_one = np.where((final_shape %2 == 0), 0, 1)
+
+    cropped = data[c[0] - to_crop[0] : c[0] + to_crop[0] + plus_one[0],
+                   c[1] - to_crop[1] : c[1] + to_crop[1] + plus_one[1],
+                   c[2] - to_crop[2] : c[2] + to_crop[2] + plus_one[2]]
+
+    return cropped
