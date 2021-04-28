53d799f8d76b8da1955ee91c6b07e062c9c19862
Clement Atlan <atlan@latlan.esrf.fr>
2021-04-28 14:37:07 +0200

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#CDIUTILS: Utilities for BCDI practionners
+
+# authors :
+#              Clement Atlan, c.atlan@outlook.com
+
+from setuptools import setup
+
+setup(name='cdiutils',version='0.1.1',
+      scripts=["cdiutils/scripts/facet_analysis.py"],
+      author="Clement Atlan")
