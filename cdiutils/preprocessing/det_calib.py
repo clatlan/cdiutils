4b92ec2faeb7e17e6d957e607acb16ae09e9f286
Clatlan <c.atlan74@gmail.com>
2022-11-15 10:16:05 +0100

    add new detector calibration function

--- cdiutils/preprocessing/det_calib.py
+++ cdiutils/preprocessing/det_calib.py
@@ -6,6 +6,104 @@ import xrayutilities as xu
 from numpy.polynomial import polynomial as P
 from scipy.ndimage import center_of_mass
 
+from cdiutils.bcdi.pipeline import pretty_print
+
+
+def detector_calibration(
+    detector_calibration_frames: np.ndarray,
+    delta: float,
+    nu: float,
+    energy: float,
+    sdd_estimate: float=None,
+    show=True,
+    verbose=True,
+):
+
+    x_com = []
+    y_com = []
+    for i in range(detector_calibration_frames.shape[0]):
+        com = center_of_mass(detector_calibration_frames[i])
+        x_com.append(com[0])
+        y_com.append(com[1])
+    
+    # get the sample to detector distance
+    # for that determine how much the the com has moved when the
+    # detector has rotated by 1 degree. We may find this value with
+    # delta or nu. Here, we do both and calculate the average. The
+    # leading coefficient of the function x_com = f(delta) gives
+    # how much the x_com has moved when delta has changed by one degree.
+
+    x_com_shift = P.polyfit(delta, x_com, 1)[1]
+    y_com_shift = P.polyfit(nu, y_com, 1)[1]
+
+    pix0_x = P.polyfit(delta, x_com, 1)[0]  # pixel 0, reference of the
+    # direct beam
+    pix0_y = P.polyfit(nu, y_com, 1)[0]
+
+    angle1, angle2 = nu, delta
+    if sdd_estimate is None:
+        sdd_estimate = (
+            (1 / 2)
+            * (1 / np.tan(np.pi / 180))
+            * (x_com_shift + y_com_shift)
+            * 55e-6
+        )
+
+    if verbose:
+        print("[INFO] First estimate of sdd: {}\n".format(sdd_estimate))
+    pretty_print(
+        "[INFO] Processing to detector calibration using area_detector_calib"
+    )
+    parameter_list, eps = xu.analysis.sample_align.area_detector_calib(
+        angle1,
+        angle2,
+        detector_calibration_frames,
+        ["z-", "y-"],
+        "x+",
+        start=(55e-6, 55e-6, sdd_estimate, 0, 0, 0, 0),
+        fix=(True, True, False, False, False, False, True),
+        wl=xu.en2lam(energy),
+    )
+
+    parameters = {
+        "cch1": parameter_list[0],
+        "cch2": parameter_list[1],
+        "pwidth1": parameter_list[2],
+        "pwidth2": parameter_list[3],
+        "distance": parameter_list[4],
+        "tiltazimuth": parameter_list[5],
+        "tilt": parameter_list[6],
+        "detrot": parameter_list[7],
+        "outerangle_offset": parameter_list[8],
+    }
+
+    if verbose:
+        pretty_print("Computed parameters")
+        for k, v in parameters.items():
+            print(
+                f"{k} = {v}"
+            )
+
+
+    
+    if show:
+        fig1, ax1 = plt.subplots(1, 1, figsize=(6, 4))
+        fig2, axes2 = plt.subplots(1, 2)
+        ax1.imshow(np.log10(detector_calibration_frames.sum(axis=0)))
+        axes2[0].plot(delta, x_com)
+        axes2[0].set_xlabel("delta")
+        axes2[0].set_ylabel("COM in x")
+
+        axes2[1].plot(nu, y_com)
+        axes2[1].set_xlabel("nu")
+        axes2[1].set_ylabel("COM in y")
+        fig1.tight_layout()
+        fig2.tight_layout()
+    
+    return parameters
+        
+
+    
 
 def det_calib(
     edf_file_template: str,
