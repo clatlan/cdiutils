51e817a75d10bfde2fb3e17f10e52f29b9ae82ea
Clatlan <c.atlan74@gmail.com>
2023-01-24 23:41:58 +0100

    update path

--- scripts/prepare_bcdi_notebook.py
+++ scripts/prepare_bcdi_notebook.py
@@ -5,12 +5,17 @@ import os
 import shutil
 import sys
 
+import cdiutils
+
 helptext =  (
     f"Usage: {sys.argv[0]} path/to/destination.ipynb"
 )
 
 if __name__ == "__main__":
-    notebook_path = os.path.dirname(__file__) + "/analyze_bcdi_data.ipynb"
+    notebook_path = (
+        os.path.dirname(cdiutils.__file__)
+        + "/../scripts/analyze_bcdi_data.ipynb"
+    )
     for arg in sys.argv[1:]:
         if not arg.endswith(".ipynb"):
             arg += ".ipynb"
@@ -19,5 +24,3 @@ if __name__ == "__main__":
             shutil.copy(notebook_path, arg)
         else:
             shutil.copy(notebook_path, os.getcwd() + "/" + arg)
-            
-    
