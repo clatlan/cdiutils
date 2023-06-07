906585e245a245a7ab02a0acf565d02a913e93d5
Clatlan <c.atlan74@gmail.com>
2023-06-07 14:29:58 +0200

    cleaning

--- cdiutils/process/processor.py
+++ cdiutils/process/processor.py
@@ -20,6 +20,7 @@ from cdiutils.utils import (
 from cdiutils.load.bliss import BlissLoader
 from cdiutils.load.spec import SpecLoader
 from cdiutils.load.sixs import SIXS2022Loader
+from cdiutils.load.p10 import P10Loader
 from cdiutils.converter import SpaceConverter
 from cdiutils.geometry import Geometry
 from cdiutils.process.phase import (
@@ -47,15 +48,21 @@ def loader_factory(metadata: dict) -> BlissLoader or SpecLoader or SIXS2022Loade
             flatfield=metadata["flatfield_path"]
 
         )
-    elif metadata["beamline_setup"] == "ID01SPEC":
+    if metadata["beamline_setup"] == "ID01SPEC":
         return SpecLoader(
             experiment_file_path=metadata["experiment_file_path"],
             detector_data_path=metadata["detector_data_path"],
             edf_file_template=metadata["edf_file_template"],
             detector_name=metadata["detector_name"]
         )
-    elif metadata["beamline_setup"] == "SIXS2022":
+    if metadata["beamline_setup"] == "SIXS2022":
         return SIXS2022Loader(
+            experiment_data_dir_path=metadata["experiment_data_dir_path"],
+            detector_name=metadata["detector_name"],
+            sample_name=metadata["sample_name"],
+        )
+    if metadata["beamline_setup"] == "P10":
+        return P10Loader(
             experiment_data_dir_path=metadata["experiment_data_dir_path"],
             detector_name=metadata["detector_name"],
             sample_name=metadata["sample_name"],
