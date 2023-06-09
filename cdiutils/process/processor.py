d4cf595e8506056734a2362152418ac4083b4cf2
Clatlan <c.atlan74@gmail.com>
2023-06-09 16:20:25 +0200

    fix bug with siunitx

--- cdiutils/process/processor.py
+++ cdiutils/process/processor.py
@@ -165,7 +165,8 @@ class BcdiProcessor:
 
     def _init_plot_parameters(self):
         update_plot_params(
-            usetex=self.params["usetex"],
+            # usetex=self.params["usetex"],
+            use_siunitx=self.params["usetex"],
             **{
                 "axes.labelsize": 7,
                 "xtick.labelsize": 6,
