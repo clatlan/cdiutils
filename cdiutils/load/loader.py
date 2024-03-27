633b365e74a4a5d86be884d4cc501d28610ee37e
Clatlan <c.atlan74@gmail.com>
2024-03-27 13:20:05 +0100

    remvoe unnecessary function and imports

--- cdiutils/load/loader.py
+++ cdiutils/load/loader.py
@@ -1,19 +1,6 @@
 """A generic class for loaders."""
 
 import numpy as np
-from typing import Callable
-<<<<<<< HEAD
-=======
-import silx.io.h5py_utils
-
-
-def safe(func: Callable) -> Callable:
-    """A wrapper to safely load data in h5 file"""
-    def wrap(self, *args, **kwargs):
-        with silx.io.h5py_utils.File(self.experiment_file_path) as self.h5file:
-            return func(self, *args, **kwargs)
-    return wrap
->>>>>>> 550ef39f68e4a988a94f2969bdd06b51b7c79f14
 
 
 class Loader:
