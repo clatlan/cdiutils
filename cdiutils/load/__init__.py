f81602580d4db27c8a49e5b637053c5f0c5f63a0
Clatlan <c.atlan74@gmail.com>
2024-03-27 10:54:50 +0100

    export import items

--- cdiutils/load/__init__.py
+++ cdiutils/load/__init__.py
@@ -3,3 +3,11 @@ from .bliss import BlissLoader
 from .p10 import P10Loader
 from .spec import SpecLoader
 from .sixs import SIXS2022Loader
+
+__all__ = [
+    "Loader",
+    "BlissLoader",
+    "P10Loader",
+    "SpecLoader",
+    "SIXS2022Loader"
+]
