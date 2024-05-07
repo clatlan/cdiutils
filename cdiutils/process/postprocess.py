f5890f5beee41d1d236440ae8d64277a98693046
Matthieu Deschamps <mdescham@hpc5-0502.esrf.fr>
2024-05-07 12:49:19 +0200

    Modification of get_structural_properties and prepare_volume in postprocess.py

--- cdiutils/process/postprocess.py
+++ cdiutils/process/postprocess.py
@@ -9,6 +9,7 @@ import numpy as np
 from numpy.fft import fftn, fftshift, ifftn, ifftshift
 from skimage.restoration import unwrap_phase
 from sklearn.linear_model import LinearRegression
+import copy
 
 from cdiutils.utils import (
     CroppingHandler,
@@ -20,6 +21,7 @@ from cdiutils.utils import (
     hybrid_gradient
 )
 
+from cdiutils.process.support_processor import SupportProcessor
 
 class PostProcessor:
     """
@@ -49,21 +51,29 @@ class PostProcessor:
             and the associated support.
         """
 
-        support = make_support(
+        support_pre_crop = make_support(
             normalize(np.abs(complex_object)),
-            isosurface=isosurface,
-            nan_values=False
+            isosurface=0.2
         )
+        final_shape_pre_crop = copy.copy(final_shape)
+        if final_shape_pre_crop is None:
+            final_shape_pre_crop = find_suitable_array_shape(support_pre_crop, padding=[6, 6, 6])
+        com_pre_crop = CroppingHandler.get_position(support_pre_crop, "com")
+        complex_object_pre_crop = CroppingHandler.force_centered_cropping(
+            complex_object,
+            where=com_pre_crop,
+            output_shape=final_shape_pre_crop
+        )
+        support_processor = SupportProcessor(parameters=parameters, data=normalize(np.abs(complex_object_pre_crop)), isosurface=isosurface, nan_values=False)
+        support, surface = support_processor.support_calculation()           
+            
         if final_shape is None:
-            final_shape = find_suitable_array_shape(
-                support,
-                padding=np.repeat(6, support.ndim)
-            )
+            final_shape = find_suitable_array_shape(support, padding=[6, 6, 6])
             print(f"[INFO] new array shape is {final_shape}")
         # center the arrays at the center of mass of the support
         com = CroppingHandler.get_position(support, "com")
         complex_object = CroppingHandler.force_centered_cropping(
-            complex_object,
+            complex_object_pre_crop,
             where=com,
             output_shape=final_shape
         )
@@ -72,7 +82,13 @@ class PostProcessor:
             where=com,
             output_shape=final_shape
         )
-        return complex_object, support
+        surface = CroppingHandler.force_centered_cropping(
+            surface,
+            where=com,
+            output_shape=final_shape
+        )
+        
+        return complex_object, support, surface
 
     @staticmethod
     def flip_reconstruction(data: np.ndarray) -> np.ndarray:
@@ -321,8 +337,10 @@ class PostProcessor:
             methods, d-spacing, lattice parameter 3D maps. hkl, g_vector
             and voxel size are also returned.
         """
-        complex_object, support = cls.prepare_volume(
-            complex_object, isosurface=isosurface)
+        complex_object, support, surface = cls.prepare_volume(
+            complex_object, isosurface=isosurface, 
+            final_shape = None, parameters=parameters
+        )
         # extract phase and amplitude
         amplitude = np.abs(complex_object)
         phase = np.angle(complex_object) * phase_factor
@@ -406,6 +424,7 @@ class PostProcessor:
         return {
             "amplitude": normalize(amplitude),
             "support": nan_to_zero(support),
+            "surface": nan_to_zero(surface),
             "phase": nan_to_zero(phase),
             "displacement": displacement,
             "displacement_gradient": displacement_gradient,
