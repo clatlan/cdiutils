fc8a23dc062d417df46dc8e25ce309d42e5a8784
Clatlan <c.atlan74@gmail.com>
2024-05-15 19:51:25 +0200

    change names of default ntoebook

--- scripts/prepare_bcdi_notebook.py
+++ scripts/prepare_bcdi_notebook.py
@@ -10,31 +10,35 @@ import cdiutils
 
 PATH, NAME = os.path.split(sys.argv[0])
 helptext = (
-    f"Usage: {NAME} path/to/destination.ipynb"
+    f"Usage: {NAME} [path/to/destination.ipynb] "
 )
 
 if __name__ == "__main__":
     notebook_path = os.path.abspath(
-        os.path.dirname(cdiutils.__file__) 
-        + "/examples/analyze_bcdi_data.ipynb"
+        os.path.dirname(cdiutils.__file__)
+        + "/examples/analyse_bcdi_data.ipynb"
     )
     if len(sys.argv) < 2:
+        sys.argv.append(".")
+    elif len(sys.argv) > 2:
         raise ValueError(helptext)
     for arg in sys.argv[1:]:
         output_path = ""
         if arg == ".":
-            output_path = os.getcwd() + "/analyze_bcdi_data.ipynb"
+            output_path = os.getcwd() + "/analyse_bcdi_data.ipynb"
         elif "/" in arg:
             path, name = os.path.split(arg)
             if not name.endswith(".ipynb"):
                 name += ".ipynb"
             if not os.path.exists(os.path.dirname(path)):
-                path = os.getcwd() + "/" + path 
+                path = os.getcwd() + "/" + path
                 if not os.path.exists(os.path.dirname(path)):
-                    raise FileExistsError(f"Diretory {path} does not exist.")
-            output_path =  path + "/" + name
+                    raise FileExistsError(
+                        f"Diretory {path} does not exist.\n" + helptext
+                    )
+            output_path = path + "/" + name
         else:
-             if not arg.endswith(".ipynb"):
-                 output_path = os.getcwd() + "/" + arg + ".ipynb"
+            if not arg.endswith(".ipynb"):
+                output_path = os.getcwd() + "/" + arg + ".ipynb"
 
         shutil.copy(notebook_path, output_path)
