a33a41e75f6716cb37abfd7b0818015401397424
Clatlan <c.atlan74@gmail.com>
2024-05-21 09:21:00 +0200

    make plot submodule avaialble

--- cdiutils/plot/__init__.py
+++ cdiutils/plot/__init__.py
@@ -2,10 +2,12 @@ from .formatting import update_plot_params
 from .formatting import get_figure_size
 from .formatting import set_plot_configs
 from .formatting import get_plot_configs
+from . import slice
 
 __all__ = [
     "update_plot_params",
     "get_figure_size",
     "set_plot_configs",
-    "get_plot_configs"
+    "get_plot_configs",
+    "slice"
 ]
