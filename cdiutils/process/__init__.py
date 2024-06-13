cf5da3168faf58cc96cda19827d72b075b83e241
Clatlan <c.atlan74@gmail.com>
2024-06-13 14:38:58 +0200

    make PhasingResultAnalyser and plot submodule available

--- cdiutils/process/__init__.py
+++ cdiutils/process/__init__.py
@@ -1,9 +1,11 @@
 from .postprocessor import PostProcessor
 from .phaser import PyNXPhaser, PhasingResultAnalyser
+from . import plot
 
 
 __all__ = [
     "PostProcessor",
     "PyNXPhaser",
-    "PhasingResultAnalyser"
+    "PhasingResultAnalyser",
+    "plot"
 ]
