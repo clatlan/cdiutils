8ac9b30ab33f9dad16153beec9f0faaf644c6f26
Clatlan <c.atlan74@gmail.com>
2024-06-19 11:40:19 +0200

    add availability at supacakge level

--- cdiutils/plot/__init__.py
+++ cdiutils/plot/__init__.py
@@ -8,6 +8,7 @@ from .formatting import x_y_lim_from_support
 from .formatting import get_x_y_limits_extents
 from .formatting import add_labels
 from .interactive import Plotter
+from .slice import plot_volume_slices
 from . import slice
 from . import volume
 
@@ -21,6 +22,7 @@ __all__ = [
     "x_y_lim_from_support",
     "get_x_y_limits_extents",
     "add_labels",
+    "plot_volume_slices",
     "slice",
     "volume",
     "Plotter"
