634bd1974c9ca95a63320d172eba8c683921cd5d
Clatlan <c.atlan74@gmail.com>
2024-07-23 17:49:56 +0200

    make BcdiPipeline and PipelinePlotter availaible at subpackage level

--- cdiutils/process/__init__.py
+++ cdiutils/process/__init__.py
@@ -1,13 +1,18 @@
-from .postprocessor import PostProcessor
-from .phaser import PyNXPhaser, PhasingResultAnalyser
+from .pipeline import BcdiPipeline, PipelinePlotter
 from .processor import BcdiProcessor
+from .phaser import PyNXPhaser, PhasingResultAnalyser
+from .postprocessor import PostProcessor
+from .parameters import get_parameters_from_notebook_variables
 from . import plot
 
 
 __all__ = [
-    "PostProcessor",
-    "PyNXPhaser",
-    "PhasingResultAnalyser",
+    "BcdiPipeline",
+    "PipelinePlotter",
     "BcdiProcessor",
+    "PyNXPhaser",
+    "PostProcessor",
+    "PhasingResultAnalyser",
+    "get_parameters_from_notebook_variables",
     "plot"
 ]
