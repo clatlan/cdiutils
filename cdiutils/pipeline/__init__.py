2fa80011160dbcc05c2c3051c6754040b4fc0506
Clatlan <c.atlan74@gmail.com>
2024-08-09 18:47:49 +0200

    pipeline related imports are now lazy

--- cdiutils/pipeline/__init__.py
+++ cdiutils/pipeline/__init__.py
@@ -4,12 +4,25 @@ Pipeline module.
 Implement the engines to run the pipelines.
 """
 
+import importlib
+
 from .bcdi_pipeline import BcdiPipeline
 from .pipeline_plotter import PipelinePlotter
 from .parameters import get_parameters_from_notebook_variables
 
-__all__ = [
-    "BcdiPipeline",
-    "PipelinePlotter",
-    "get_parameters_from_notebook_variables",
-]
+
+__class_func_submodules__ = {
+    "BcdiPipeline": "bcdi_pipeline",
+    "PipelinePlotter": "pipeline_plotter",
+    "get_parameters_from_notebook_variables": "parameters"
+}
+
+__all__ = list(__class_func_submodules__)
+
+def __getattr__(name):
+    if name in __class_func_submodules__:
+        submodule = importlib.import_module(
+            f"{__name__}.{__class_func_submodules__[name]}"
+        )
+        return getattr(submodule, name)
+    raise AttributeError(f"module {__name__} has no attribute {name}.")
