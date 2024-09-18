1cff29e694bc53928b04eff63f0bb91e66619920
Clatlan <c.atlan74@gmail.com>
2024-09-18 14:16:09 +0200

    change parameters key word to params

--- cdiutils/pipeline/__init__.py
+++ cdiutils/pipeline/__init__.py
@@ -8,13 +8,13 @@ import importlib
 
 from .bcdi_pipeline import BcdiPipeline
 from .pipeline_plotter import PipelinePlotter
-from .parameters import get_parameters_from_notebook_variables
+from .parameters import get_params_from_notebook_variables
 
 
 __class_func_submodules__ = {
     "BcdiPipeline": "bcdi_pipeline",
     "PipelinePlotter": "pipeline_plotter",
-    "get_parameters_from_notebook_variables": "parameters"
+    "get_params_from_notebook_variables": "parameters"
 }
 
 __all__ = list(__class_func_submodules__)
