e74c922542c472f989ed32f79103b6022b7a7bdb
Clatlan <c.atlan74@gmail.com>
2024-09-23 16:26:22 +0200

    better handling of the logger/prints

--- cdiutils/pipeline/base.py
+++ cdiutils/pipeline/base.py
@@ -20,15 +20,21 @@ class LoggerWriter:
     Custom stream to send stdout (print statements) directly to
     logger in real-time.
     """
-    def __init__(self, logger, level):
+    def __init__(self, logger, level, wrap=True):
         self.logger = logger
         self.level = level
+        self.wrap = wrap
 
     def write(self, message):
         if message.strip():
             # Only log if there's something to log
             # (ignores empty messages)
-            self.logger.log(self.level, message.strip())
+            if self.wrap:
+                # Wrap lines at 79 characters
+                wrapped_message = textwrap.fill(message.strip(), width=79)
+            else:
+                wrapped_message = message.strip()
+            self.logger.log(self.level, "\n" + wrapped_message + "\n")
 
     def flush(self):
         """
@@ -37,56 +43,30 @@ class LoggerWriter:
         pass
 
 
-def process(func: Callable) -> Callable:
-    @wraps(func)
-    def wrapper(self, *args, **kwargs) -> None:
-
-        # Setup a new log file for this process
-        file_handler = self._init_process_logger(
-            f"{self.dump_dir}/{func.__name__}_output"
-        )
-        self.logger.info(f"Starting process: {func.__name__}")
-
-        # Redirect stdout to capture print statements in real time
-        original_stdout = sys.stdout  # Save original stdout
-        sys.stdout = LoggerWriter(self.logger, logging.INFO)
-
-        try:
-            func(self, *args, **kwargs)
-            self.logger.info(
-                f"Process {func.__name__} completed successfully."
-            )
-        except Exception as e:
-            self.logger.error(
-                "\nError occurred in the "
-                f"'{func.__name__}' process:\n{e}"
-            )
-            # traceback.print_exception(e)
-            raise
-        finally:
-            # Restore original stdout and remove file handler
-            sys.stdout = original_stdout
-            self.logger.removeHandler(file_handler)
-            file_handler.close()
-    return wrapper
-
-
-def pretty_print(text: str, max_char_per_line: int = 79) -> None:
+def pretty_print(
+        text: str,
+        max_char_per_line: int = 79,
+        do_print: bool = True,
+        return_text: bool = False
+) -> None | str:
     """Print text with a frame of stars."""
-
     pretty_text = "\n".join(
         [
             "",
-            "*" * (max_char_per_line + 4),
+            "*" * (max_char_per_line),
             *[
-                f"* {w[::-1].center(max_char_per_line)[::-1]} *"
-                for w in textwrap.wrap(text, width=max_char_per_line)
+                f"* {w[::-1].center(max_char_per_line-4)[::-1]} *"
+                for w in textwrap.wrap(text, width=max_char_per_line - 4)
             ],
-            "*" * (max_char_per_line + 4),
+            "*" * max_char_per_line,
             "",
         ]
     )
-    print(pretty_text)
+    if do_print:
+        print(pretty_text)
+    if return_text:
+        return pretty_text
+    return None
 
 
 class Pipeline(ABC):
@@ -113,7 +93,7 @@ class Pipeline(ABC):
                 raise ValueError(
                     "param_file_path or parameters must be provided"
                 )
-            # self.params = self.load_parameters()
+            self.params = self.load_parameters()
 
         self.dump_dir = self.params["dump_dir"]
 
@@ -126,6 +106,45 @@ class Pipeline(ABC):
 
         # update the plot parameters
         update_plot_params()
+
+    @staticmethod
+    def process(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(self, *args, **kwargs) -> None:
+
+            # Setup a new log file for this process
+            file_handler = self._init_process_logger(
+                f"{self.dump_dir}/{func.__name__}_output"
+            )
+            msg = pretty_print(
+                f"Starting process: {func.__name__}",
+                do_print=False,
+                return_text=True
+            )
+            self.logger.info(msg)
+
+            # Redirect stdout to capture print statements in real time
+            original_stdout = sys.stdout  # Save original stdout
+            sys.stdout = LoggerWriter(self.logger, logging.INFO)
+
+            try:
+                func(self, *args, **kwargs)
+                self.logger.info(
+                    f"Process {func.__name__} completed successfully."
+                )
+            except Exception as e:
+                self.logger.error(
+                    "\nError occurred in the "
+                    f"'{func.__name__}' process:\n{e}"
+                )
+                # traceback.print_exception(e)
+                raise
+            finally:
+                # Restore original stdout and remove file handler
+                sys.stdout = original_stdout
+                self.logger.removeHandler(file_handler)
+                file_handler.close()
+        return wrapper
 
     def _make_dump_dir(self) -> None:
         dump_dir = self.params["dump_dir"]
@@ -143,21 +162,27 @@ class Pipeline(ABC):
             )
 
     def _init_logger(self) -> logging.Logger:
+        # Remove all handlers associated with the root logger (Jupyter
+        # default).
+        for handler in logging.root.handlers[:]:
+            logging.root.removeHandler(handler)
+
         logger = logging.getLogger("PipelineLogger")
 
-        # Check if the logger already has handlers to avoid adding multiple
-        # if not logger.hasHandlers():
-        logger.setLevel(logging.DEBUG)
+        # Check if the logger already has handlers to avoid adding
+        # multiple.
+        if not logger.hasHandlers():
+            logger.setLevel(logging.DEBUG)
 
-        # Console handler
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
+            # Console handler
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.INFO)
 
-        console_formatter = logging.Formatter(
-            fmt="[%(levelname)s] %(message)s",
-        )
-        console_handler.setFormatter(console_formatter)
-        logger.addHandler(console_handler)
+            console_formatter = logging.Formatter(
+                fmt="[%(levelname)s] %(message)s",
+            )
+            console_handler.setFormatter(console_formatter)
+            logger.addHandler(console_handler)
 
         return logger
 
