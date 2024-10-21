1008acf8cf0c9bef1122d8a17b947e700513664b
Clatlan <c.atlan74@gmail.com>
2024-10-21 10:53:34 +0200

    add name and repr method in geometry

--- cdiutils/geometry.py
+++ cdiutils/geometry.py
@@ -17,11 +17,12 @@ class Geometry:
     """
     def __init__(
             self,
-            sample_circles: list,
-            detector_circles: list,
+            sample_circles: list = None,
+            detector_circles: list = None,
             detector_vertical_orientation: str = "y-",
             detector_horizontal_orientation: str = "x+",
-            beam_direction: list = None
+            beam_direction: list = None,
+            name: str = None
     ) -> None:
         self.sample_circles = sample_circles
         self.detector_circles = detector_circles
@@ -31,6 +32,8 @@ class Geometry:
             self.beam_direction = [1, 0, 0]
         else:
             self.beam_direction = beam_direction
+
+        self.name = name
 
         self.is_cxi = True
 
@@ -45,7 +48,8 @@ class Geometry:
                 detector_circles=["y-", "x-"],  # nu, delta
                 detector_vertical_orientation="y-",
                 detector_horizontal_orientation="x+",
-                beam_direction=[1, 0, 0]
+                beam_direction=[1, 0, 0],
+                name="ID01"
             )
         if "p10" in beamline_setup.lower():
             return cls(
@@ -53,7 +57,8 @@ class Geometry:
                 detector_circles=["y+", "x-"],  # gam, del (or e2_t02)
                 detector_vertical_orientation="y-",
                 detector_horizontal_orientation="x+",
-                beam_direction=[1, 0, 0]
+                beam_direction=[1, 0, 0],
+                name="P10"
             )
         if beamline_setup.lower() == "sixs2022":
             return cls(
@@ -61,7 +66,8 @@ class Geometry:
                 detector_circles=["y+", "x-"],  # gamma, delta  NOT SURE OF THE COMMENT
                 detector_vertical_orientation="y-",
                 detector_horizontal_orientation="x+",
-                beam_direction=[1, 0, 0]
+                beam_direction=[1, 0, 0],
+                name="SIXS2022"
             )
         if beamline_setup.lower() == "nanomax":
             return cls(
@@ -69,7 +75,8 @@ class Geometry:
                 detector_circles=["y-", "x-"],  # gamma, delta
                 detector_vertical_orientation="y+",
                 detector_horizontal_orientation="x-",
-                beam_direction=[1, 0, 0]
+                beam_direction=[1, 0, 0],
+                name="NanoMAX"
             )
         if beamline_setup.lower() == "cristal":
             return cls(
@@ -77,7 +84,16 @@ class Geometry:
                 detector_circles=["y+", "x-"],  # gamma, delta  OK FOR omega/delta but not for the two others
                 detector_vertical_orientation="y-",
                 detector_horizontal_orientation="x+",
-                beam_direction=[1, 0, 0]
+                beam_direction=[1, 0, 0],
+                name="CRISTAL"
+            )
+
+        if beamline_setup.lower() == "id27":
+            return cls(
+                detector_vertical_orientation="y-",
+                detector_horizontal_orientation="x-",
+                beam_direction=[1, 0, 0],
+                name="ID27"
             )
         raise NotImplementedError(
             f"The beamline_setup {beamline_setup} is not valid. Available:\n"
@@ -102,3 +118,14 @@ class Geometry:
             self.detector_horizontal_orientation
         ]
         self.is_cxi = False
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.name} geometry:\n"
+            f"{self.sample_circles = }\n"
+            f"{self.detector_circles = }\n"
+            f"{self.detector_vertical_orientation = }\n"
+            f"{self.detector_horizontal_orientation = }\n"
+            f"{self.beam_direction = }\n"
+            f"{self.is_cxi = }\n"
+        )
