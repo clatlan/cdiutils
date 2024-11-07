05c54181905904a0b9c6b9b0e8e9499899e063e1
clatlan <c.atlan@outlook.com>
2024-11-07 15:31:26 +0100

    remove imports

--- cdiutils/process/__init__.py
+++ cdiutils/process/__init__.py
@@ -1,13 +1,9 @@
-from .processor import BcdiProcessor
 from .phaser import PyNXPhaser, PhasingResultAnalyser
 from .postprocessor import PostProcessor
-from . import plot
 
 
 __all__ = [
-    "BcdiProcessor",
     "PyNXPhaser",
     "PostProcessor",
     "PhasingResultAnalyser",
-    "plot"
 ]
