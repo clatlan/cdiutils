355c1eb50c8ad13ccae957cddf506ea89e02ef4d
Clement Atlan <clement.atlan@esrf.fr>
2025-10-24 18:14:16 +0200

    add new class to handle Support

--- src/cdiutils/process/__init__.py
+++ src/cdiutils/process/__init__.py
@@ -1,9 +1,11 @@
 from .phaser import PyNXPhaser, PhasingResultAnalyser
 from .postprocessor import PostProcessor
+from .support_tools import SupportTools
 
 
 __all__ = [
     "PyNXPhaser",
     "PostProcessor",
     "PhasingResultAnalyser",
+    "SupportTools",
 ]
