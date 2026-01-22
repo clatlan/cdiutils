0f013600a2f19f5e8e8f77ecc985a522c01e55d7
Abd-zak <aboudi.zak00@@gmail.com>
2026-01-22 13:59:51 +0100

    ruff formating for  MultiVolumeViewer layout, styling, and controls

--- src/cdiutils/interactive/multiviewer_3d.py
+++ src/cdiutils/interactive/multiviewer_3d.py
@@ -68,9 +68,6 @@ Dependencies
 The widget is intended for interactive use inside Jupyter environments.
 """
 
-
-
-
 import ipywidgets as widgets
 import numpy as np
 import plotly.graph_objects as go
